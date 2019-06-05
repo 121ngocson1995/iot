3c7cf80ef349fe58291d2b3614668a1f5c9276f0
Ngoc Son <ngocson@pop-os.localdomain>
2019-06-06 02:06:19 +0700

    update android app

--- android/app/src/main/java/com/example/anew/MainActivity.java
+++ android/app/src/main/java/com/example/anew/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.anew;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,12 +26,21 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference();
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
 
-    DatabaseReference status = myRef.child("opened");
+    DatabaseReference doorStatus = database.child("opened");
+    DatabaseReference lockStatus = database.child("locked");
+    DatabaseReference alarmStatus = database.child("ringingstatus");
+
+    String locked = "";
+    String alarm = "";
+    String opened = "";
 
     TextView textView;
+    Button lockBtn;
+    Button disableAlarmBtn;
+
+    ColorStateList oldTextViewColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +48,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textView);
+        lockBtn = (Button) findViewById(R.id.button_lock);
+        disableAlarmBtn = (Button) findViewById(R.id.button_disableAlarm);
 
-        Log.d("file", "Connected to firebase");
+        oldTextViewColor =  textView.getTextColors();
 
         FirebaseMessaging.getInstance().subscribeToTopic("Alarm").addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
@@ -47,19 +62,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        status.addValueEventListener(new ValueEventListener() {
+        doorStatus.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String opened = dataSnapshot.getValue(String.class);
+                opened = dataSnapshot.getValue(String.class);
 
-                if (opened.equals("true")) {
-                    textView.setText("UNLOCKED");
-                    textView.setTextColor(Color.RED);
-                } else {
-                    textView.setText("LOCKED");
-                    textView.setTextColor(Color.GREEN);
+                if (alarm.equals("true") == false) {
+                    if (opened.equals("true")) {
+                        textView.setText("OPENED");
+                        textView.setTextColor(Color.rgb(255,140,0));
+                    } else {
+                        textView.setText("CLOSED");
+                        textView.setTextColor(Color.GREEN);
+                    }
                 }
-                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50);
+
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 60);
             }
 
             @Override
@@ -68,5 +86,95 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("file", "Failed to read value.", error.toException());
             }
         });
+
+        lockStatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                locked = dataSnapshot.getValue(String.class);
+
+                if (locked.equals("true")) {
+                    lockBtn.setText("Unlock the door");
+                } else {
+                    lockBtn.setText("Lock the door");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        alarmStatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                alarm = dataSnapshot.getValue(String.class);
+
+                if (alarm.equals("true")) {
+                    disableAlarmBtn.setEnabled(true);
+
+                    textView.setText("BREACHED");
+                    textView.setTextColor(Color.RED);
+
+                    Animation anim = new AlphaAnimation(0.0f, 1.0f);
+                    anim.setDuration(500); //You can manage the blinking time with this parameter
+                    anim.setStartOffset(20);
+                    anim.setRepeatMode(Animation.REVERSE);
+                    anim.setRepeatCount(Animation.INFINITE);
+                    textView.startAnimation(anim);
+                } else {
+                    disableAlarmBtn.setEnabled(false);
+
+                    textView.clearAnimation();
+
+                    textView.setText("Fetching status...");
+                    textView.setTextColor(oldTextViewColor );
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+
+                    new CountDownTimer(3000, 3000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            // do nothing
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            if (opened.equals("true")) {
+                                textView.setText("OPENED");
+                                textView.setTextColor(Color.rgb(255,140,0));
+                            } else {
+                                textView.setText("CLOSED");
+                                textView.setTextColor(Color.GREEN);
+                            }
+
+                            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 60);
+                        }
+                    }.start();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    public void toggleLock(View v) {
+        if (locked.equals("true")) {
+            lockStatus.setValue("false");
+        } else {
+            lockStatus.setValue("true");
+        }
+    }
+
+    public void toggleAlarm(View v) {
+        if (alarm.equals("true")) {
+            alarmStatus.setValue("false");
+        } else {
+            alarmStatus.setValue("true");
+        }
     }
 }
