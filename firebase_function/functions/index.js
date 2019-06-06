0cf99e321620b29ce44a346432669d8573a64410
Ngoc Son <ngocson@pop-os.localdomain>
2019-06-07 01:31:17 +0700

    update firebase function

--- firebase_function/functions/index.js
+++ firebase_function/functions/index.js
@@ -5,24 +5,35 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.sendNotification = functions.database.ref('/opened').onWrite((change, context) => {
-  // Determine the condition
-  if (change.before.val() === "false" && change.after.val() === "true") {
-    // Notification details.
-    payload = {
-      notification: {
-        title: 'Someone entered your house!',
-        body: 'Please take neccessary actions',
-        badge: '1',
-        sound: 'default'
-      }
-    };
+  const eventBefore = change.before;
+  const eventAfter = change.after;
 
-    // Send notification to topic
-    // It will be sent to all devices subscribing to the same topic
-    return admin.messaging().sendToTopic('Alarm', payload).then(response => {
-      console.log('Notification success: ', response);
-    }).catch(error => {
-      console.log('Notification error: ', error);
+  // Determine the condition
+  if (eventBefore.val() === 'false'
+      && eventAfter.val() === 'true') {
+    return eventAfter.ref.parent.child('locked').once('value').then(locked => {
+      console.log('AAAAAA: ' + locked.val());
+      if (locked.val() === 'true') {
+        // Notification details.
+        const payload = {
+          notification: {
+            title: 'Someone entered your house!',
+            body: 'Please take neccessary actions',
+            badge: '1',
+            sound: 'default'
+          }
+        };
+
+        // Send notification to topic
+        // It will be sent to all devices subscribing to the same topic
+        return admin.messaging().sendToTopic('Alarm', payload).then(response => {
+          console.log('Notification success: ', response);
+        }).catch(error => {
+          console.log('Notification error: ', error);
+        });
+      }
+
+      return true;
     });
   }
 
